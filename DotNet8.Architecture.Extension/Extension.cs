576283e0a51c396db44cb8d68b64b1f2593a7d0a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-19 21:55:27 +0630

    Format Code

--- DotNet8.Architecture.Extension/Extension.cs
+++ DotNet8.Architecture.Extension/Extension.cs
@@ -6,38 +6,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNet8.Architecture.Extension
+namespace DotNet8.Architecture.Extension;
+
+public static class Extension
 {
-	public static class Extension
+
+	#region ToModel
+
+	public static BlogModel ToModel(this TblBlog dataModel)
 	{
-
-		#region ToModel
-
-		public static BlogModel ToModel(this TblBlog dataModel)
+		return new BlogModel
 		{
-			return new BlogModel
-			{
-				BlogId = dataModel.BlogId,
-				BlogTitle = dataModel.BlogTitle,
-				BlogAuthor = dataModel.BlogAuthor,
-				BlogContent = dataModel.BlogContent,
-			};
-		}
-
-		#endregion
-
-		#region ToEntity
-
-		public static TblBlog ToEntity(this BlogRequestModel model)
-		{
-			return new TblBlog
-			{
-				BlogTitle = model.BlogTitle,
-				BlogAuthor = model.BlogAuthor,
-				BlogContent = model.BlogContent
-			};
-		}
-
-		#endregion
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+		};
 	}
+
+	#endregion
+
+	#region ToEntity
+
+	public static TblBlog ToEntity(this BlogRequestModel model)
+	{
+		return new TblBlog
+		{
+			BlogTitle = model.BlogTitle,
+			BlogAuthor = model.BlogAuthor,
+			BlogContent = model.BlogContent
+		};
+	}
+
+	#endregion
 }
