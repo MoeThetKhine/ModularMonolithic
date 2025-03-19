850942870000a12dfc67bd7dafecf4c3bf0201f5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-19 15:48:21 +0630

    -

--- DotNet8.Architecture.DTOs/Feature/Blog/BlogListModel.cs
+++ DotNet8.Architecture.DTOs/Feature/Blog/BlogListModel.cs
@@ -1,9 +1,4 @@
 ﻿using DotNet8.Architecture.DTOs.Feature.PageSetting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.Architecture.DTOs.Feature.Blog
 {
