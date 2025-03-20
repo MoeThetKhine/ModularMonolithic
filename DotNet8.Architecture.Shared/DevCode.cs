201b9f109b483d71ef8d209924096a5d59486d25
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-20 11:51:40 +0630

    Feat :  Add DevCode

---
+++ DotNet8.Architecture.Shared/DevCode.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architecture.Shared
+{
+	public static class DevCode
+	{
+		public static string ToJson(this object obj)=>
+			JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+		public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
+
+		public static bool IsNullOrEmpty(this string str) =>
+	   string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+
+		public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageNo, int pageSize)
+		{
+			return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
+		}
+	}
+}
