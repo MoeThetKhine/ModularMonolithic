fef9ad2fd0c92d01fb7b79d346993ded1c99e081
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-20 20:55:28 +0630

    Format Code

--- DotNet8.Architecture.Utils/Result.cs
+++ DotNet8.Architecture.Utils/Result.cs
@@ -1,93 +1,92 @@
 ﻿using DotNet8.Architecture.Utils.Enums;
 
-namespace DotNet8.Architecture.Utils
+namespace DotNet8.Architecture.Utils;
+
+public class Result<T>
 {
-	public class Result<T>
+	public T Data { get; set; }
+	public string Message { get; set; }
+	public bool IsSuccess { get; set; }
+	public EnumStatusCode StatusCode { get; set; }
+
+	public static Result<T> Success(string message = "Success", EnumStatusCode statusCode = EnumStatusCode.Success)
 	{
-		public T Data { get; set; }
-		public string Message { get; set; }
-		public bool IsSuccess { get; set; }
-		public EnumStatusCode StatusCode { get; set; }
-
-		public static Result<T> Success(string message = "Success", EnumStatusCode statusCode = EnumStatusCode.Success)
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = true,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 
-		public static Result<T> Success(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	public static Result<T> Success(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				Data = data,
-				IsSuccess = true,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			Data = data,
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 
-		public static Result<T> SaveSuccess(string message = "Saving Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	public static Result<T> SaveSuccess(string message = "Saving Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = true,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 
-		public static Result<T> UpdateSuccess(string message = "Update Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	public static Result<T> UpdateSuccess(string message = "Update Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = true,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 
-		public static Result<T> DeleteSuccess(string message = "Delete Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	public static Result<T> DeleteSuccess(string message = "Delete Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = true,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 
-		public static Result<T> Fail(string message = "Failed.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+	public static Result<T> Fail(string message = "Failed.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = false,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			IsSuccess = false,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 
-		public static Result<T> Failure(Exception ex)
+	public static Result<T> Failure(Exception ex)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = false,
-				Message = ex.Message,
-				StatusCode = EnumStatusCode.InternalServerError
-			};
-		}
+			IsSuccess = false,
+			Message = ex.Message,
+			StatusCode = EnumStatusCode.InternalServerError
+		};
+	}
 
-		public static Result<T> NotFound(string message = "No Data Found.", EnumStatusCode statusCode = EnumStatusCode.NotFound)
+	public static Result<T> NotFound(string message = "No Data Found.", EnumStatusCode statusCode = EnumStatusCode.NotFound)
+	{
+		return new Result<T>
 		{
-			return new Result<T>
-			{
-				IsSuccess = false,
-				Message = message,
-				StatusCode = statusCode
-			};
-		}
+			IsSuccess = false,
+			Message = message,
+			StatusCode = statusCode
+		};
 	}
 }
