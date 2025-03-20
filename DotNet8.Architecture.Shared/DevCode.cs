f8874b8d875f09b92b312fc271c7c8fa8bbe7668
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-20 11:52:37 +0630

    Format Code

--- DotNet8.Architecture.Shared/DevCode.cs
+++ DotNet8.Architecture.Shared/DevCode.cs
@@ -1,18 +1,17 @@
-﻿namespace DotNet8.Architecture.Shared
+﻿namespace DotNet8.Architecture.Shared;
+
+public static class DevCode
 {
-	public static class DevCode
+	public static string ToJson(this object obj)=>
+		JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+	public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
+
+	public static bool IsNullOrEmpty(this string str) =>
+   string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+
+	public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageNo, int pageSize)
 	{
-		public static string ToJson(this object obj)=>
-			JsonConvert.SerializeObject(obj, Formatting.Indented);
-
-		public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
-
-		public static bool IsNullOrEmpty(this string str) =>
-	   string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-
-		public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageNo, int pageSize)
-		{
-			return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
-		}
+		return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
 	}
 }
