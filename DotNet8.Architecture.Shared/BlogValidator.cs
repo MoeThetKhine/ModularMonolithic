e643e25ab047b2e545129832a4bfe4a4b5ad49e9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-20 11:49:13 +0630

    Feat : Global Using

--- DotNet8.Architecture.Shared/BlogValidator.cs
+++ DotNet8.Architecture.Shared/BlogValidator.cs
@@ -1,7 +1,4 @@
-﻿using DotNet8.Architecture.DTOs.Feature.Blog;
-using FluentValidation;
-
-namespace DotNet8.Architecture.Shared;
+﻿namespace DotNet8.Architecture.Shared;
 
 public class BlogValidator : AbstractValidator<BlogRequestModel>
 {
