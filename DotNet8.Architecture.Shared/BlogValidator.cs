72c738fd1bea6405ecbf6fa283a8dec4ca2f1ed1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-20 11:48:04 +0630

    Format Code

--- DotNet8.Architecture.Shared/BlogValidator.cs
+++ DotNet8.Architecture.Shared/BlogValidator.cs
@@ -1,28 +1,27 @@
 ﻿using DotNet8.Architecture.DTOs.Feature.Blog;
 using FluentValidation;
 
-namespace DotNet8.Architecture.Shared
+namespace DotNet8.Architecture.Shared;
+
+public class BlogValidator : AbstractValidator<BlogRequestModel>
 {
-	public class BlogValidator : AbstractValidator<BlogRequestModel>
+
+	#region BlogValidator
+
+	public BlogValidator()
 	{
+		RuleFor(x => x.BlogTitle)
+			.NotEmpty().WithMessage("Blog Title cannot be empty.")
+			.NotNull().WithMessage("Blog Title cannot be null.");
 
-		#region BlogValidator
+		RuleFor(x => x.BlogAuthor)
+			.NotEmpty().WithMessage("Blog Author cannot be empty.")
+		   .NotNull().WithMessage("Blog Author cannot be null.");
 
-		public BlogValidator()
-		{
-			RuleFor(x => x.BlogTitle)
-				.NotEmpty().WithMessage("Blog Title cannot be empty.")
-				.NotNull().WithMessage("Blog Title cannot be null.");
-
-			RuleFor(x => x.BlogAuthor)
-				.NotEmpty().WithMessage("Blog Author cannot be empty.")
-			   .NotNull().WithMessage("Blog Author cannot be null.");
-
-			RuleFor(x => x.BlogContent)
-			  .NotEmpty().WithMessage("Blog Content cannot be empty.")
-			  .NotNull().WithMessage("Blog Content cannot be null.");
-		}
-
-		#endregion
+		RuleFor(x => x.BlogContent)
+		  .NotEmpty().WithMessage("Blog Content cannot be empty.")
+		  .NotNull().WithMessage("Blog Content cannot be null.");
 	}
+
+	#endregion
 }
