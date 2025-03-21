de9a6d6cd0d1eb0c645bf5c1325f637128f66abb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 12:47:45 +0630

    Feat : Add BlogRepository

--- DotNet8.Modules.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Modules.Infrastructure/Features/Blog/BlogRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DotNet8.Architecture.DbService.AppDbContextModels;
+using DotNet8.Architecture.DTOs.Feature.Blog;
+using DotNet8.Architecture.Utils;
+using DotNet8.Modules.Domain.Features.Blog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,18 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Modules.Infrastructure.Features.Blog
 {
-	internal class BlogRepository
+	public class BlogRepository : IBlogRepository
 	{
+		private readonly AppDbContext _context;
+
+		public BlogRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
