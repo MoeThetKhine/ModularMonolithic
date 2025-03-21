2e8cccb1282973965fdf90be29c26a2bde4d9757
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 12:30:22 +0630

    Format Code

--- DotNet8.Modules.Domain/Features/Blog/Tbl_Blog.cs
+++ DotNet8.Modules.Domain/Features/Blog/Tbl_Blog.cs
@@ -5,24 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNet8.Modules.Domain.Features.Blog
+namespace DotNet8.Modules.Domain.Features.Blog;
+
+
+#region Tbl_Blog
+
+public class Tbl_Blog
 {
+	[Key]
+	public long BlogId { get; set; }
 
-	#region Tbl_Blog
+	public string BlogTitle { get; set; } = null!;
 
-	public class Tbl_Blog
-	{
-		[Key]
-		public long BlogId { get; set; }
+	public string BlogAuthor { get; set; } = null!;
 
-		public string BlogTitle { get; set; } = null!;
+	public string BlogContent { get; set; } = null!;
 
-		public string BlogAuthor { get; set; } = null!;
-
-		public string BlogContent { get; set; } = null!;
-
-		public bool? DeleteFlag { get; set; }
-	}
-
-	#endregion
+	public bool? DeleteFlag { get; set; }
 }
+
+#endregion
