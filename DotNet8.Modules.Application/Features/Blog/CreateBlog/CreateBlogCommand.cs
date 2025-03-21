05cdb26df7da8bcea588da43bc196447c85cbb87
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 20:50:24 +0630

    Feat : Add CreateBlogCommand

--- DotNet8.Modules.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
+++ DotNet8.Modules.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
@@ -1,5 +1,11 @@
 ﻿namespace DotNet8.Modules.Application.Features.Blog.CreateBlog;
 
-public class CreateBlogCommand
+public class CreateBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel requestModel;
+
+	public CreateBlogCommand(BlogRequestModel requestModel)
+	{
+		this.requestModel = requestModel;
+	}
 }
