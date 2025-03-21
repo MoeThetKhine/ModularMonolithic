16b85b0bfd32743ede03e42a8b14ad30b41751fb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 12:51:55 +0630

    Format Code

--- DotNet8.Modules.Presentation/Extensions/DependencyInjection.cs
+++ DotNet8.Modules.Presentation/Extensions/DependencyInjection.cs
@@ -3,35 +3,33 @@ using DotNet8.Modules.Domain.Features.Blog;
 using DotNet8.Modules.Infrastructure.Features.Blog;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Modules.Presentation.Extensions
+namespace DotNet8.Modules.Presentation.Extensions;
+
+public static class DependencyInjection
 {
-	public static class DependencyInjection
+	private static IServiceCollection AddDbContextServices(this  IServiceCollection services, WebApplicationBuilder builder)
 	{
-		private static IServiceCollection AddDbContextServices(this  IServiceCollection services, WebApplicationBuilder builder)
+		builder.Services.AddDbContext<AppDbContext>(
+		opt =>
 		{
-			builder.Services.AddDbContext<AppDbContext>(
-			opt =>
-			{
-				opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-				opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-			},
-			ServiceLifetime.Transient,
-			ServiceLifetime.Transient
-			);
+			opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+			opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+		},
+		ServiceLifetime.Transient,
+		ServiceLifetime.Transient
+		);
 
-			return services;
-		}
-
-		private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-		{
-			return services.AddScoped<IBlogRepository, BlogRepository>();
-		}
-
-		public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			return services.AddDbContextServices(builder)
-				.AddRepositoryService();
-		}
+		return services;
 	}
 
+	private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+	{
+		return services.AddScoped<IBlogRepository, BlogRepository>();
+	}
+
+	public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		return services.AddDbContextServices(builder)
+			.AddRepositoryService();
+	}
 }
