4b7d45512b499940da1b5a9f212e1c06738b94fa
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 13:46:02 +0630

    Format Code

--- DotNet8.Modules.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ DotNet8.Modules.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Modules.Application.Features.Blog.PatchBlog
+﻿namespace DotNet8.Modules.Application.Features.Blog.PatchBlog;
+
+internal class PatchBlogCommandHandler
 {
-	internal class PatchBlogCommandHandler
-	{
-	}
 }
