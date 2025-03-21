2b1d96dbd443b5fa59cf190de1e69f3b039e1018
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 12:29:47 +0630

    Feat : Add Tbl_Blog

--- DotNet8.Modules.Domain/Features/Blog/Tbl_Blog.cs
+++ DotNet8.Modules.Domain/Features/Blog/Tbl_Blog.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotNet8.Modules.Domain.Features.Blog
 {
-	internal class Tbl_Blog
+	public class Tbl_Blog
 	{
+		[Key]
+		public long BlogId { get; set; }
+
+		public string BlogTitle { get; set; } = null!;
+
+		public string BlogAuthor { get; set; } = null!;
+
+		public string BlogContent { get; set; } = null!;
+
+		public bool? DeleteFlag { get; set; }
 	}
 }
