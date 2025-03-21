566ef25d7455e5a77a1c18523ab87b4aa9cb680b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-21 13:49:15 +0630

    Format Code

--- DotNet8.Modules.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.Modules.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Modules.Application.Features.Blog.CreateBlog
+﻿namespace DotNet8.Modules.Application.Features.Blog.CreateBlog;
+
+internal class CreateBlogCommandHandler
 {
-	internal class CreateBlogCommandHandler
-	{
-	}
 }
