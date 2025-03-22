696cf6ddb8cde1aaf001d222ff2d287b42c18f10
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 20:51:26 +0630

    Feat : Add DeleteBlogCommandHandler

--- DotNet8.Modules.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.Modules.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,5 +1,28 @@
 ﻿namespace DotNet8.Modules.Application.Features.Blog.DeleteBlog;
 
-public class DeleteBlogCommandHandler
+public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>>
 {
+	private readonly IBlogRepository _blogRepository;
+
+	public DeleteBlogCommandHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogRepository.DeleteBlogAsync(request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
+
 }
