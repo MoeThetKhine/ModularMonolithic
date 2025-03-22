62db24374ca6b9a687440c772ada3f0af739d70f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 16:50:27 +0630

    Feat : Add PatchBlogCommand

--- DotNet8.Modules.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ DotNet8.Modules.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -1,5 +1,15 @@
 ﻿namespace DotNet8.Modules.Application.Features.Blog.PatchBlog;
 
-public class PatchBlogCommand
+public class PatchBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel BlogRequestModel { get; set; }
+
+	public int BlogId { get; set; }
+
+
+	public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
+	{
+		BlogRequestModel = blogRequestModel;
+		BlogId = blogId;
+	}
 }
