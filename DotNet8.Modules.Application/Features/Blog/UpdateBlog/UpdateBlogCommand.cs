fe23d983e520768fa7b9d8a407a75adb1c9cc81b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 16:02:41 +0630

    Feat : Add UpdateBlogAsync in UpdateBlogCommand

--- DotNet8.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,5 +1,15 @@
 ﻿namespace DotNet8.Modules.Application.Features.Blog.UpdateBlog;
 
-public class UpdateBlogCommand
+public class UpdateBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel RequestModel { get; set; }
+
+	public int BlogId { get; set; }
+
+	public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
+	{
+		RequestModel = requestModel;
+		BlogId = blogId;
+	}
+
 }
