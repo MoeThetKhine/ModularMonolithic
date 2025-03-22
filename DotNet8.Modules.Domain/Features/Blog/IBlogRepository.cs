f7beae1639dbc7d1aa25d51095e5b90dc5e617ab
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 16:55:50 +0630

    -

--- DotNet8.Modules.Domain/Features/Blog/IBlogRepository.cs
+++ DotNet8.Modules.Domain/Features/Blog/IBlogRepository.cs
@@ -8,7 +8,7 @@ public interface IBlogRepository
 	Task<Result<BlogModel>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
 	Task<Result<BlogModel>> CreateBlogAsync(BlogRequestModel blogRequest, CancellationToken cancellationToken);
 	Task<Result<BlogModel>> UpdateBlogAsync(BlogRequestModel blogRequest,int id , CancellationToken cancellationToken);
-	Task<Result<BlogModel>> PatchBlogAsync(BlogRequestModel requestModel, int id, CancellationToken cancellationToken)
+	Task<Result<BlogModel>> PatchBlogAsync(BlogRequestModel requestModel, int id, CancellationToken cancellationToken);
 }
 
 #endregion
