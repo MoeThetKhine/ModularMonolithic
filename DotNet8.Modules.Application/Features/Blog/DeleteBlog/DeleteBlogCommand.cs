4ff6eefcceb41075c2ad8208231b7302d640acc2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 20:51:12 +0630

    Feat : Add DeleteBlogCommand

--- DotNet8.Modules.Application/Features/Blog/DeleteBlog/DeleteBlogCommand.cs
+++ DotNet8.Modules.Application/Features/Blog/DeleteBlog/DeleteBlogCommand.cs
@@ -1,5 +1,11 @@
 ﻿namespace DotNet8.Modules.Application.Features.Blog.DeleteBlog;
 
-public class DeleteBlogCommand
+public class DeleteBlogCommand : IRequest<Result<BlogModel>>
 {
+	public int BlogId {  get; set; }	
+
+	public DeleteBlogCommand(int blogId)
+	{
+		BlogId = blogId;
+	}
 }
