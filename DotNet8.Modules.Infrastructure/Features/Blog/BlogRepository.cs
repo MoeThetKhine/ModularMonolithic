40f5976777d8a1f6243732e0cabacf3180334572
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-22 20:50:57 +0630

    Feat : Add DeleteBlogAsync in BlogRepository

--- DotNet8.Modules.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Modules.Infrastructure/Features/Blog/BlogRepository.cs
@@ -199,4 +199,33 @@ public class BlogRepository : IBlogRepository
 
 	#endregion
 
+	public async Task<Result<BlogModel>> DeleteBlogAsync(int id, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		try
+		{
+			var blog = await _context.TblBlogs.FindAsync([id, cancellationToken], cancellationToken: cancellationToken);
+
+			if (blog is null)
+			{
+				result = Result<BlogModel>.NotFound();
+				goto result;
+			}
+
+			_context.TblBlogs.Remove(blog);
+			await _context.SaveChangesAsync(cancellationToken);
+
+			result = Result<BlogModel>.DeleteSuccess();
+		}
+
+		catch (Exception ex)
+		{
+			result = Result<BlogModel>.Failure(ex);
+		}
+
+	result:
+		return result;
+	}
+
 }
